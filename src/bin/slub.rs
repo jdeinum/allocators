c65c9402293695e0cd187485f9fada682dd95bf4
Jacob Deinum <jdeinum@nullspaces.org>
2025-08-12 21:13:39 -0600

    just a bit of time so docs

--- src/bin/slub.rs
+++ src/bin/slub.rs
@@ -1,3 +1,22 @@
+// The kernel slub allocator works by providing preallocated objects of a particular size. The
+// first time you request an object of a particular size, a cache is started for objects of that
+// size. The next time you require an object of that size, it is served from the cache instead,
+// which means we save ourselves a few pages of allocated memory.
+//
+// Similar to the glic allocator, when we are all done with our memory, we just return it to the
+// userspace cache rather than returning the memory to the operating system.
+//
+// Because all of our messages are the same size, the Slub allocator should reduce the number of
+// allocations since we are never really dealing with more than one message at a time. Not very
+// representative i know... but if the cache is large enough the principle should be the same.
+//
+// In applications where there are many objects with distinct sizes, the slub allocator would be a
+// poor choice because we'd be wasting a bunch of space.
+//
+// One final thing to note is that the kernel typically uses an arena allocator for really large
+// objects, so the amount of fragmentation decreases if we aren't actually using all of the large
+// objects in the cache.
+
 use anyhow::{Context, Result};
 use std::{
     alloc::{GlobalAlloc, System},
@@ -25,10 +44,16 @@ impl SlubAllocator {
 // but would be nice
 unsafe impl GlobalAlloc for SlubAllocator {
     unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        // check the size
+
+        // if we have seen the size before, serve it from the cache
+
+        // if not, start a cache, and serve from there
         todo!()
     }
     unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
-        todo!()
+
+        // return the item to the cache
     }
 }
 
